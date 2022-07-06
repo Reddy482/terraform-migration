06bfe244aa27e90bc43d2305889758ade83fcfd9
Reddy482 <67957670+Reddy482@users.noreply.github.com>
2022-07-06 14:22:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,4 @@
-terraform {
-  required_version = ">=0.14.5"
-  backend "s3" {
-    bucket         = "test-migration-to-tfe"
-    key            = "test/migration.tfstate"
-    region         = "us-east-1"
-  }
-}
+
 provider "aws" {
   region = "us-east-1"
 }
