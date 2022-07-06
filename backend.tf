d8d538b4a658798d4c537d774a002959da5a4744
Reddy482 <67957670+Reddy482@users.noreply.github.com>
2022-07-06 14:32:07 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,21 @@
 # Configure backend
 
+# terraform {
+#   backend "s3" {
+#     bucket = "my-s3-bucket-sridhar"
+#     key    = "test/migration.tfstate"
+#     region = "us-east-1"
+#   }
+# }
+
+
 terraform {
-  backend "s3" {
-    bucket = "my-s3-bucket-sridhar"
-    key    = "test/migration.tfstate"
-    region = "us-east-1"
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "rsr"
+
+    workspaces {
+      name = "terraform-migration"
+    }
   }
 }
