ad996947164f13f5fb96708b30ba26d6152225a5
Reddy482 <67957670+Reddy482@users.noreply.github.com>
2022-07-06 16:20:57 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,12 +1,12 @@
 # Configure backend
 
-terraform {
-  backend "s3" {
-    bucket = "my-s3-bucket-sridhar"
-    key    = "test/migration1.tfstate"
-    region = "us-east-1"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "my-s3-bucket-sridhar"
+#     key    = "test/migration1.tfstate"
+#     region = "us-east-1"
+#   }
+# }
 
 # provider "github" {
 #   # token = "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"
@@ -14,35 +14,15 @@ terraform {
 
 provider "github" {}
 
-# terraform {
-#   required_providers {
-#     github = {
-#       source  = "integrations/github"
-#       version = "~> 4.0"
-#     }
-#   }
-# }
+terraform {
+  cloud {
+    organization = "rsr"
+
+    workspaces {
+      name = "test-migration"
+    }
+  }
+}
 
 
-# terraform {
-#   backend "remote" {
-#     hostname = "app.terraform.io"
-#     organization = "rsr"
 
-#     workspaces {
-#       name = "terraform-migration"
-#     }
-#     token = "2hG5GbISLXEGyQ.atlasv1.GIqhk2EcQuYyIIQKM7KE7f1py0oqJrc2CZ9HbNY5H3ENnWMPIUZBOHKnQvW0zrE8D70"
-#   }
-# }
-
-
-# terraform {
-#   cloud {
-#     organization = "rsr"
-
-#     workspaces {
-#       name = "test-integration"
-#     }
-#   }
-# }
