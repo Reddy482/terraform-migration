a743dbe29acfe06edf5531ccfd8310f9a7766c79
Reddy482 <67957670+Reddy482@users.noreply.github.com>
2022-07-06 15:30:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
+terraform {
+      required_providers {
+         aws = {
+         source = "hashicorp/aws"
+         version = "= 3.74.2"
+        }
+     }
+  }
 
 resource "aws_instance" "foo" {
   ami           = "ami-0cff7528ff583bf9a" # us-west-2
